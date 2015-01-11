f7f118c5ad3d8296bac9650df4b0d1c8a103db4b
Thomas Taylor <tom@taylortom.co.uk>
2015-01-11 00:37:47 +0000

    Add basic renaming of albums

--- loadAlbums.js
+++ loadAlbums.js
@@ -79,11 +79,15 @@ function handleIncomingRequest(req, res) {
 
     req.parsedUrl = url.parse(req.url, true);
     var coreUrl = req.parsedUrl.pathname;
+    var method = req.method.toLowerCase();
 
-    if(coreUrl == "/albums.json") {
+    if(coreUrl == "/albums.json" && method == "get") {
         handleListAlbums(req, res);
     }
-    else if(coreUrl.substr(0,7) == "/albums" && coreUrl.substr(req.url.length-5 == ".json")) {
+    else if(coreUrl.substr(coreUrl.length-12) == "/rename.json" && method == "post") {
+        handleRenameAlbum(req, res);
+    }
+    else if(coreUrl.substr(0,7) == "/albums" && coreUrl.substr(req.url.length-5 == ".json") && method == "get") {
         handleGetAlbum(req, res);
     }
     else {
@@ -121,6 +125,61 @@ function handleGetAlbum(req, res) {
     });
 }
 
+function handleRenameAlbum(req, res) {
+    var coreUrl = req.parsedUrl.pathname;
+    var parts = coreUrl.split("/");
+    if(parts.length != 4) {
+        sendFailure(res, 404, invalidResource(coreUrl));
+    }
+    else {
+        var albumName = parts[2];
+        var jsonBody = "";
+
+        req.on("readable", function() {
+            var d = req.read();
+            if(d) {
+                if(typeof d == "string") {
+                    jsonBody += d;
+                }
+                else if(typeof d == "object" && d instanceof Buffer) {
+                    jsonBody += d.toString("utf8");
+                }
+            }
+        });
+
+        req.on("end", function() {
+            if(jsonBody) {
+                try {
+                    var albumData = JSON.parse(jsonBody);
+                    if(!albumData.albumName) {
+                        sendFailure(res, 403, missingData("album_name"));
+                        return
+                    }
+                }
+                catch(e) {
+                    sendFailure(res, 403, badJson());
+                    return;
+                }
+
+                doRename(albumName, albumData.albumName, function(err, results) {
+                    if(err) {
+                        if(err.code == "ENOENT") sendFailure(res, 403, noSuchAlbum());
+                        else sendFailure(res, 500, fileError(err));
+                    }
+                    else sendSuccess(res, null);
+                });
+            }
+            else {
+                sendFailure(res, 403, badJson());
+            }
+        });
+    }
+}
+
+function doRename(oldName, newName, callback) {
+    fs.rename("albums/" + oldName, "albums/" + newName, callback);
+}
+
 function makeError(err, msg) {
     var e = new Error(msg);
     e.code = err;
@@ -136,10 +195,7 @@ function sendSuccess(res, data) {
 function sendFailure(res, code, err) {
     var errCode = (err.code) ? err.code : err.name;
     res.writeHead(code, { "Content-Type": "application/json" });
-    res.end(JSON.stringify({
-        error: errCode,
-        message: err.message
-    }) + "\n");
+    res.end(JSON.stringify({ error: errCode, message: err.message }) + "\n");
 }
 
 function invalidResource() {
@@ -148,6 +204,20 @@ function invalidResource() {
 
 function noSuchAlbum() {
     return makeError("no_such_album", "the specified album does not exist");
+}
+
+function file_error(err) {
+    var msg = "There was a file error on the server: " + err.message;
+    return makeError("server_file_error", msg);
+}
+
+function missingData(missing) {
+    var msg = "Your request is missing" + (missing ? ": '" + missing + "'" : " some data.");
+    return makeError("missing_data", msg);
+}
+
+function badJson() {
+    return makeError("invalid_json", "the provided data is not valid JSON");
 }
 
 var s = http.createServer(handleIncomingRequest);
