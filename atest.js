5220c90c9817ed4c48daee59c1d72d506f1edbb5
Thomas Taylor <thomas.taylor@kineo.com>
2015-01-14 00:39:53 +0000

    Refactor atest.js to use async module

--- atest.js
+++ atest.js
@@ -1,6 +1,13 @@
+var async = require("async");
 var amgr = require("./album_mgr");
 
-amgr.albums("./", function(err, albums) {
+// regular
+//amgr.albums("./", regularCb);
+
+// async
+amgr.albums("./", asyncCb);
+
+function regularCb(err, albums) {
     if(err) {
         console.log("Unexpected error: " + JSON.stringify(err));
     }
@@ -24,4 +31,28 @@ amgr.albums("./", function(err, albums) {
             }
         })(0);
     }
-});
+};
+
+function asyncCb(err, albums) {
+    if(err) {
+        console.log("Unexpected error: " + JSON.stringify(err));
+    }
+    else {
+        async.forEach(albums, function(album, callback) {
+            album.photos(function(err, photos) {
+                if(err) {
+                    console.log("Error loading album: " + JSON.stringify(err));
+                }
+                else {
+                    console.log(album.name);
+                    console.log(photos);
+                    console.log("");
+                }
+            });
+            callback(null);
+        }, function(err){
+            if(err) console.log(err);
+            else console.log("Done");
+        });
+    }
+};
